c0e5e3e08a29d51d076724aa90cd98862acb2162
ymk1t <yuma1102eff@gmail.com>
2020-06-14 01:05:56 +0900

    Initial Commit

---
+++ CalculatorApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CalculatorApp
+//
+//  Created by 小西裕真 on 2020/06/14.
+//  Copyright © 2020 小西裕真. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
